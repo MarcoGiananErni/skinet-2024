66265dceeed6f0c01058281e17072e326b875966
MarcoGiananErni <marco.gianan@betterask.erni>
2026-06-04 11:43:47 +0800

    Refactor repository structure: add IGenericRepository interface and GenericRepository implementation; clean up unused usings in ProductsController and Program files

--- API/Program.cs
+++ API/Program.cs
@@ -11,10 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-    
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
